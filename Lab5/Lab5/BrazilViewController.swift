5d5a4f08bc5d459e15d99ff3b6e0aca523b570a1
Taity-mini <1504693@rgu.ac.uk>
2016-10-30 15:16:26 +0000

    Lab 5 Completed
    Multi Scene Web view project

---
+++ Lab5/Lab5/BrazilViewController.swift
@@ -0,0 +1,39 @@
+//
+//  BrazilViewController.swift
+//  Lab5
+//
+//  Created by Andrew Tait on 28/10/2016.
+//  Copyright © 2016 Andrew Tait. All rights reserved.
+//
+
+import UIKit
+
+class BrazilViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        let url = NSURL (string: "https://en.wikipedia.org/wiki/Brazil")
+        let requestObj = NSURLRequest(URL: url!);
+        webView.loadRequest(requestObj)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Lab5/Lab5Tests/Lab5Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Lab5Tests.swift
+//  Lab5Tests
+//
+//  Created by Andrew Tait on 28/10/2016.
+//  Copyright © 2016 Andrew Tait. All rights reserved.
+//
+
+import XCTest
+@testable import Lab5
+
+class Lab5Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
